2f70652de9fb8a6d7f0aad9161d1b790d2f50d7d
PachydermX <renjimin@gmail.com>
2015-07-09 14:16:32 +0900

    Basic Work

---
+++ is0243kv/week05/js/communicator.js
@@ -0,0 +1,85 @@
+/**
+ * TextChat 
+ * @constructor
+ * @param url - WebSocketを呼び出すURL
+ * @param console - ログを出力するconsoleエリア
+ * @param updateUICallback - WebSocketの状態が変わった時に呼び出すコールバック関数
+ */
+function Communicator(url, console, updateUICallback) {
+  this.url = url;
+  this.socket = null;
+  this.from = '';
+  // jQueryのオブジェクトにしておく
+  this.$console = $(console);
+  // コールバック関数が渡されない時は，ダミーの関数を設定しておく
+  this.updateUICallback = updateUICallback || function() {};
+}
+
+// JSON形式のメッセージを送信する
+// この時，fromを追加してから送る
+Communicator.prototype.sendJsonMsg = function(jsonMsg) {
+  if (this.socket != null && this.socket.readyState === 1) {
+    jsonMsg.from = this.from;
+    this.socket.send(JSON.stringify(jsonMsg));
+  } else {
+    alert("WebSocket is not opened");
+  }
+};
+
+// 受信したJSON形式のメッセージを処理する
+Communicator.prototype.handleJsonMsg = function(jsonMsg) {
+  if (jsonMsg.type === 'message') {
+    var text = jsonMsg.from + ' > ' + jsonMsg.text;
+    this.outputToConsole(text);
+	  add_enemy("enemyA", 50, "weapon");
+  } else if (jsonMsg.type === 'clearAll') {
+    this.$console.empty();
+  }
+};
+
+// コンソールのエリアにテキストを追加する．オプションで色を指定できるようにしている．
+Communicator.prototype.outputToConsole = function(text, color) {
+  var p = $('<p></p>').text(text);
+  if (color) {
+    // CSSを使って色を指定する
+    p.css('color', color);
+  }
+  this.$console.prepend(p);
+};
+
+// connectする際にfromに入れるnameを指定する
+Communicator.prototype.connect = function(name) {
+  this.from = name;
+  this.socket = new WebSocket(this.url);
+  // TextChatのオブジェクトをコールバック関数で参照できるようにする 
+  var that = this;
+  // WebSocketをオープンした時の処理
+  this.socket.onopen = function() {
+    that.outputToConsole('WebSocket opened.', 'salmon');
+    // UI状態を更新する
+    that.updateUICallback();
+  };
+  
+  // WebSocketにメッセージが届いた時の処理
+  this.socket.onmessage = function(message) {
+    // JSON形式でメッセージが送られてくる
+    var msg = JSON.parse(message.data);
+    that.handleJsonMsg(msg);
+  };
+  
+  // WebSocketがクローズした時の処理
+  this.socket.onclose = function() {
+    that.outputToConsole('WebSocket closed.', 'salmon');
+    that.socket = null;
+    that.updateUICallback();
+  };
+};
+
+Communicator.prototype.disconnect = function() {
+  if (this.socket != null) {
+    this.socket.close();
+    this.socket = null;
+  } else {
+    alert('WebSocket is not opened.');
+  }
+};
--- is0243kv/week05/js/game.js
+++ is0243kv/week05/js/game.js
@@ -28,12 +28,76 @@ $(document).ready(function () {
         $("#console").text(e.keyCode + " Up");
     });
     
-    // configure
+    // configure game
     game = $("#game");
     player = new player();
 	player.x = size[0] / 2;
 	player.y = size[1] - 100;
     player.init($("#player"), null);
+	
+	// configure communicator
+	  // WebSocketを呼び出すURL
+	  var url = 'ws://' + document.location.host;
+	  // consoleに対応するHTML要素
+	  var console = document.getElementById('console');
+	  // ボタンの状態を更新する関数
+	  function updateButtons() {
+		if (communicator.socket == null) {
+		  // WebSocketが開いていなければ，コネクトボタンを有効にする
+		  $('#connect_button').prop('disabled', false);
+		  $('#name').prop('disabled', false);
+		  $('#disconnect_button').prop('disabled', true);
+		  $('#send_button').prop('disabled', true);
+		} else {
+		  // WebSocketが開いている場合
+		  $('#connect_button').prop('disabled', true);
+		  $('#name').prop('disabled', true);
+		  $('#disconnect_button').prop('disabled', false);
+		  $('#send_button').prop('disabled', false);
+		}
+	  };
+
+	  communicator = new Communicator(url, console, updateButtons);
+
+	  // メッセージの入力エリアのデータを送信する
+	  function sendMessage() {
+		communicator.sendJsonMsg({
+		  type: 'message',
+		  text: $('#message').val()
+		});
+	  }
+
+	  $('#connect_button').on('click', function() {
+		communicator.connect($('#name').val());
+	  });
+
+	  $('#disconnect_button').on('click', function() {
+		communicator.disconnect();
+	  });
+
+	  $('#send_button').on('click', function() {
+		sendMessage();
+	  });
+
+	  $('#message').on('keypress', function(event) {
+		// エンターキーを入力した時にメッセージを送る
+		if (event.keyCode == 13) {
+		  sendMessage();
+		}
+	  });
+
+	  $('#clear_all_button').on('click', function() {
+		communicator.sendJsonMsg({type: 'clearAll'});
+	  });
+
+	  // 他のブラウザとできるだけ衝突しないように名前の初期値を設定する
+	  $('#name').attr('value', (function() {
+		var d = new Date();
+		return 'user' + (d.getTime() % 10000);
+	  })());
+
+	  // ボタンの表示を更新する
+	  updateButtons();
     
     start_loop();
     
